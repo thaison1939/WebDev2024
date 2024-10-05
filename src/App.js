1343162d912706c9ed996e286aa38aa56414c7e7
NguyenVuThe <nguyenvtk2110@gmail.com>
2024-10-05 14:42:41 +0700

    Add Today's Featured Site

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import QuestionList from './components/QuestionList/QuestionList';
 import questionsData from './_SAMPLE_DATA/questions.json';
+
 import Tag from './components/MiniTag/Tag';
-import tagData from './_SAMPLE_DATA/tagName.json'
+import tagData from './_SAMPLE_DATA/tagName.json';
+
+import TodayFeaturedSite from './components/TodayFeaturedSite/TodayFeaturedSite';
+import siteData from './_SAMPLE_DATA/todayfeaturedsite.json';
 
 function App() {
   const [questions, setQuestions] = useState([]);
@@ -17,10 +21,20 @@ function App() {
       <div>
         <QuestionList questions={questions} />
 
-        {/* Tags */}
         <div> {tagData.tags.map((tag, index) => (
             <Tag key={index} name_tag={tag}/> 
         ))}
+        </div>
+
+        <div>
+            <TodayFeaturedSite
+              image={siteData.image}
+              title={siteData.title}
+              paragraph={siteData.paragraph}
+              questions={siteData.questions}
+              answers={siteData.answers}
+              answered={siteData.answered}
+            />
         </div>
 
       </div>
