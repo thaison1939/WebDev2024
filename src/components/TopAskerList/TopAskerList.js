ae721aa60877a5dd7c2090770b241f2535ef7250
YukiMakoto25 <tamnd.22bi13400@usth.edu.vn>
2024-10-09 21:34:10 +0700

    Adjust TopAskerList position

--- src/components/TopAskerList/TopAskerList.js
+++ src/components/TopAskerList/TopAskerList.js
@@ -4,22 +4,20 @@ import style from './TopAskerList.module.scss'
 
 const TopAskerList = ({ askers }) => {
     return (
-        <div>
+        <div className={style["asker-list-container"]}>
             <div className={style["heading"]}>Top Network Askers</div>
-            <div className={style["asker-list-container"]}>
-                <ul>
-                    {askers.map((asker, index) => (
-                        <li key={index} className={style["asker-list-item"]}>
-                            <Asker
-                                image={asker.image}
-                                name={asker.name}
-                                field={asker.field}
-                                link={asker.link}
-                            />
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <ul>
+                {askers.map((asker, index) => (
+                    <li key={index} className={style["asker-list-item"]}>
+                        <Asker
+                            image={asker.image}
+                            name={asker.name}
+                            field={asker.field}
+                            link={asker.link}
+                        />
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
