f0523236bb72f8c625e6d986d8eb7df39acdcdbd
Kwang1402 <hongquang2k4@gmail.com>
2024-10-11 00:33:37 +0700

    Modify Question component

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import styles from './Question.module.scss';
+import Tag from '../Tag/Tag';
 
-const Question = ({ image, title, description, link }) => {
+const Question = ({ image, title, link, tags, answer, created_time, author, field}) => {
     return (
         <div className={styles.container}>
             <img src={image} alt={title} className={styles["question-image"]} />
-            <div className={styles["question-details"]}>
+            <div className={styles["question-content"]}>
                 <a href={link} className={styles["question-link"]}>
                     <div className={styles["question-title"]}>{title}</div>
                 </a>
-                <div className={styles["question-description"]}>{description}</div>
+                <div className={styles["question-details"]}>
+                    <div className={styles["tags"]}>
+                        {tags.map((tag, index) => (
+                            <Tag key={index} name_tag={tag} />
+                        ))}
+                    </div>
+                    <div className={styles["details"]}>
+                        {answer} answers | asked {new Date(created_time).toLocaleDateString()} by {author} in {field}
+                    </div>
+                </div>
             </div>
         </div>
     );
