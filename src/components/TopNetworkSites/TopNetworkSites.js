1131bf5ac1cda4f3b4f161c22e55825464b10f81
Ngô Quý Đăng Tuấn <dangtuannq@gmail.com>
2024-10-11 22:45:30 +0700

    Modify TopNetworkSites using Hook

--- src/components/TopNetworkSites/TopNetworkSites.js
+++ src/components/TopNetworkSites/TopNetworkSites.js
@@ -1,55 +1,29 @@
-import React from "react";
-import styles from'./TopNetworkSites.module.scss';
+import React, { useState } from "react";
+import styles from './TopNetworkSites.module.scss';
+import linksData from '../../_SAMPLE_DATA/links.json';
 
-function TopNetworkSites(){
+function TopNetworkSites() {
+    const [links] = useState(linksData);
+
     return (
         <div className={styles.container}>
             <div className={styles["heading"]}>Top Network Sites</div>
             <nav className={styles["topnetworksites-nav-links"]}>
-                <a href = "https://stackoverflow.com/" className={styles["topnetworksites-link"]}>
-                    <img className={styles["topnetworksites-logo"]} src="https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico" alt = "Stack Overflow logo"/>
-                     Stack Overflow 
-                </a>
-                <a href = "https://math.stackexchange.com/" className={styles["topnetworksites-link"]}>
-                    <img className={styles["topnetworksites-logo"]} src="https://cdn.sstatic.net/Sites/math/Img/favicon.ico" alt="Mathematics logo"/>
-                     Mathematics 
-                </a>
-                <a href = "https://superuser.com/" className={styles["topnetworksites-link"]}>
-                    <img className={styles["topnetworksites-logo"]} src="https://cdn.sstatic.net/Sites/superuser/Img/favicon.ico" alt="Super User logo"/>
-                     Super User 
-                </a>
-                <a href = "https://askubuntu.com/" className={styles["topnetworksites-link"]}>
-                    <img className={styles["topnetworksites-logo"]} src="https://cdn.sstatic.net/Sites/askubuntu/Img/favicon.ico" alt="Ask Ubuntu logo"/>
-                     Ask Ubuntu 
-                </a>
-                <a href = "https://meta.stackexchange.com/" className={styles["topnetworksites-link"]}>
-                    <img className={styles["topnetworksites-logo"]} src="https://meta.stackexchange.com/Content/Sites/stackexchangemeta/Img/favicon.ico" alt="Meta Stack Exchange logo"/>
-                     Meta Stack Exchange 
-                </a>
-                <a href = "https://serverfault.com/" className={styles["topnetworksites-link"]}>
-                    <img className={styles["topnetworksites-logo"]} src="https://cdn.sstatic.net/Sites/serverfault/Img/favicon.ico" alt="Server Fault logo"/>
-                     Server Fault 
-                </a>
-                <a href = "https://unix.stackexchange.com/" className={styles["topnetworksites-link"]}>
-                    <img className={styles["topnetworksites-logo"]} src="https://cdn.sstatic.net/Sites/unix/Img/favicon.ico" alt="Unix & Linux logo"/>
-                     Unix & Linux 
-                </a>
-                <a href = "https://ru.stackoverflow.com/" className={styles["topnetworksites-link"]}>
-                    <img className={styles["topnetworksites-logo"]} src="https://cdn.sstatic.net/Sites/ru/Img/favicon.ico" alt="Stack Overflow на русском logo "/>
-                     Stack Overflow на русском 
-                </a>
-                <a href = "https://english.stackexchange.com/" className={styles["topnetworksites-link"]}>
-                    <img className={styles["topnetworksites-logo"]} src="https://cdn.sstatic.net/Sites/english/Img/favicon.ico?v=678003fec3e2" alt="English Language & Usage logo"/>
-                     English Language & Usage 
-                </a>
-                <a href = "https://physics.stackexchange.com/" className={styles["topnetworksites-link"]}>
-                    <img className={styles["topnetworksites-logo"]} src="https://cdn.sstatic.net/Sites/physics/Img/favicon.ico?v=678003fec3e2" alt="Physics logo"/>
-                     Physics 
-                </a>
+                {links.map(link => (
+                    <a key={link.href} href={link.href} className={styles["topnetworksites-link"]}>
+                        <img
+                            className={styles["topnetworksites-logo"]}
+                            src={link.imgSrc}
+                            alt={link.altText}
+                        />
+                        {link.name}
+                    </a>
+                ))}
             </nav>
+
             <p className={styles["topnetworksites-footer"]}>
-                <a href = "https://stackexchange.com/sites" className={styles["topnetworksites-footer"]}>
-                    <b> View all 182 sites »</b>
+                <a href="https://stackexchange.com/sites" className={styles["topnetworksites-footer"]}>
+                    <b>View all 182 sites »</b>
                 </a>
             </p>
         </div>
