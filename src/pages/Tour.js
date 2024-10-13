de28549c5282f0c93c5a1dfe08b3b29632006df8
Ngô Quý Đăng Tuấn <dangtuannq@gmail.com>
2024-10-13 22:16:44 +0700

    For solving conflict

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import UnlockBadges from '../components/UnlockBadges/UnlockBadges';
-import NewUser from '../components/NewUser/NewUser';
-
+import React, {useState, useEffect} from 'react';
+import AskQuestion from '../components/TourAnimation/AskQuestion/AskQuestion.js';
+import TourComponentList from '../components/TourAnimation/TourList/TourComponentList.js';
+import toursData from '../_SAMPLE_DATA/toursData.json';
 
 const Tour = () => {
+    const [tours, setTours] = useState([]);
+
+    useEffect(() => {
+        const toursArray = Object.values(toursData); 
+        setTours(toursArray); 
+    }, []); 
+
     return (
-        <div>
-            {/* <h1>Due on Saturday!</h1> */}
-            <UnlockBadges/>
-            <NewUser/>
-        </div>
+        <>
+            <h1>Due on Saturday!</h1>
+            <AskQuestion/>
+            <TourComponentList tours = {tours}/>
+        </>
     );
 };
 
 export default Tour;
- 
