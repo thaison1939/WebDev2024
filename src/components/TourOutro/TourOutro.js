575beeec024e0a059686e9bcede837f9c1c5a26a
unknown <thangkay3009@gmail.com>
2024-10-14 08:30:45 +0700

    modify Tour

--- src/components/TourOutro/TourOutro.js
+++ src/components/TourOutro/TourOutro.js
@@ -1,14 +1,20 @@
 import React from "react";
 import styles from "./TourOutro.module.scss";
 
-const WelcomeStackExchange = ({
-  title,
-  image,
-}) => {
+const WelcomeStackExchange = ({ outro }) => {
+  const title = outro.title;
+  const image = outro.image;
   return (
     <div className={styles.container}>
       <h2 className={styles.h2}>{title}</h2>
-      <div className={styles.description}>In 2008, Jeff Atwood and Joel Spolsky launched Stack Overflow, a site where programmers could help solve each other's problems. Online. For free. Since then, millions of people have jumped at the chance to help a stranger. And thanks to those people like you, the Stack Exchange network has grown to include 182 different communities visited by over 100 million monthly unique visitors.</div>
+      <div className={styles.description}>
+        In 2008, Jeff Atwood and Joel Spolsky launched Stack Overflow, a site
+        where programmers could help solve each other's problems. Online. For
+        free. Since then, millions of people have jumped at the chance to help a
+        stranger. And thanks to those people like you, the Stack Exchange
+        network has grown to include 182 different communities visited by over
+        100 million monthly unique visitors.
+      </div>
       <div className={styles["logo-container"]}>
         <img src={image} alt="Stack Exchange logo" className={styles.logo} />
       </div>
--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,78 +1,59 @@
-import React, {useState, useEffect} from 'react';
-import styles from './Tour.module.scss';
-import Navbar from '../components/Navbar/Navbar';
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
-import AskQuestion from '../components/TourAnimation/AskQuestion/AskQuestion.js';
-import TourIntro from '../components/TourIntro/TourIntro.js';
-import TourOutro from '../components/TourOutro/TourOutro.js';
-import TourComponentList from '../components/TourAnimation/TourList/TourComponentList.js';
-import IntroData from '../_SAMPLE_DATA/TourIntro.json';
-import OutroData from '../_SAMPLE_DATA/TourOutro.json';
-import toursData from '../_SAMPLE_DATA/toursData.json';
-import NewUser from '../components/NewUser/NewUser.js';
+import React, { useState, useEffect } from "react";
+import styles from "./Tour.module.scss";
+import Navbar from "../components/Navbar/Navbar";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import AskQuestion from "../components/TourAnimation/AskQuestion/AskQuestion.js";
+import TourIntro from "../components/TourIntro/TourIntro.js";
+import TourOutro from "../components/TourOutro/TourOutro.js";
+import TourComponentList from "../components/TourAnimation/TourList/TourComponentList.js";
+import IntroData from "../_SAMPLE_DATA/TourIntro.json";
+import OutroData from "../_SAMPLE_DATA/TourOutro.json";
+import toursData from "../_SAMPLE_DATA/toursData.json";
+import NewUser from "../components/NewUser/NewUser.js";
 
 const Tour = () => {
-    const [intro, setIntro] = useState([]);
+  const [intro, setIntro] = useState([]);
 
-    useEffect(() => {
-        const introArray = Object.values(IntroData); 
-        setIntro(introArray); 
-    }, []); 
+  useEffect(() => {
+    const intro = IntroData["intro"];
+    setIntro(intro);
+  }, []);
+  const [outro, setOutro] = useState([]);
 
-    const [outro, setOutro] = useState([]);
+  useEffect(() => {
+    const outro = OutroData["outro"];
+    setOutro(outro);
+  }, []);
+  const [tours, setTours] = useState([]);
 
-    useEffect(() => {
-        const outroArray = Object.values(OutroData); 
-        setOutro(outroArray); 
-    }, []); 
+  useEffect(() => {
+    const toursArray = Object.values(toursData["tour"]);
+    setTours(toursArray);
+  }, []);
 
-    const [tours, setTours] = useState([]);
-
-    useEffect(() => {
-        const toursArray = Object.values(toursData["tour"]); 
-        setTours(toursArray); 
-    }, []); 
-
-    return (
-        <div className={styles.container}>
-            <Navbar />
-            <div className= {styles["header"]}>
-                <div className={styles["wrapper"]}>
-                    <Header />
-                </div>  
-            </div>
-            <div className={styles["content"]}>
-                <div className={styles["wrapper"]}> 
-                    {/*Intro*/}
-                    {intro.map((intro, index) => (
-                        <TourIntro
-                            key={index}
-                            title={intro.title}
-                            image={intro.image}
-                            additional_info={intro.additional_info}
-                        />
-                    ))}
-                    <AskQuestion/>
-                    <TourComponentList tours = {tours}/>
-                    <NewUser/>
-                    {/*Outro*/}
-                    {outro.map((outro, index) => (
-                        <TourOutro
-                            key={index}
-                            title={outro.title}
-                            image={outro.image}
-                            additional_info={outro.additional_info}
-                        />
-                        ))}
-                </div>                    
-            </div>
-            
-            <Footer />
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <div className={styles["header"]}>
+        <div className={styles["wrapper"]}>
+          <Header />
         </div>
-        );
+      </div>
+      <div className={styles["content"]}>
+        <div className={styles["wrapper"]}>
+          {/*Intro*/}
+            <TourIntro intro={intro} />
+          <AskQuestion />
+          <TourComponentList tours={tours} />
+          <NewUser />
+            <TourOutro outro={outro} />
+        </div>
+      </div>
 
+      <Footer />
+    </div>
+  );
 };
 
 export default Tour;
- 
