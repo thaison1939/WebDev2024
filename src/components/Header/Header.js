f8a43c060e2808858947f5b3407a8bae681cbfa2
Kwang1402 <hongquang2k4@gmail.com>
2024-10-18 14:15:35 +0700

    add new logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <header className={styles["site-header"]}>
       <div>
         <a href ="http://localhost:3000/">
-          <h1>Stack Exchange</h1>
+          <h1>USTH Exchange</h1>
         </a>
       </div>
       <ul className={styles["header-nav-links"]}>
